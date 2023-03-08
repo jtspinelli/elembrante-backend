b69c8d9583c3f7529446c2320cf17dddf7783679
Jonathan <contato@jonathanspinelli.com>
2023-03-08 14:39:24 -0300

    add dotenv-safe

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express, { json, Request, Response } from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv-safe';
 
 const port = process.env.PORT || 3000;
 
 const app = express();
-
 app.use(json());
 app.use(cors());
 
-app.get('/', (_req: Request, res: Response) => res.send("Hello!"));
+dotenv.config();
+
+app.get('/', (_req: Request, res: Response) => res.send("Hello!") );
 
 app.listen(port, () => console.log("APP RUNNING ON PORT " + port));
