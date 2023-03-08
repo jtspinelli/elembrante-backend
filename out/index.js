82b88e39a0e0ff27c12389eccfe866e22ebd6ad5
jtspinelli <contato@jonathanspinelli.com>
2023-03-08 19:45:08 -0300

    testing

--- out/index.js
+++ out/index.js
@@ -28,13 +28,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importStar(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const dotenv_safe_1 = __importDefault(require("dotenv-safe"));
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const port = process.env.PORT || 3000;
 const app = (0, express_1.default)();
 app.use((0, express_1.json)());
 app.use((0, cors_1.default)());
-dotenv_safe_1.default.config();
+// dotenv.config();
 app.get('/', (_req, res) => {
     res.send("Hello!\nSECRET: " + process.env.SECRET);
 });
