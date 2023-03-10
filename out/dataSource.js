0591a594cd0e1b35ee04386bf3305618adfc7b7b
jtspinelli <contato@jonathanspinelli.com>
2023-03-10 09:04:04 -0300

    comment dotenv.config

--- out/dataSource.js
+++ out/dataSource.js
@@ -1,13 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
-const dotenv_safe_1 = __importDefault(require("dotenv-safe"));
 const typeorm_1 = require("typeorm");
 const Usuario_1 = require("./entity/Usuario");
-dotenv_safe_1.default.config();
+//dotenv.config();
 const host = process.env.HOST;
 const username = process.env.HOSTUSERNAME;
 const password = process.env.HOSTPASSWORD;
