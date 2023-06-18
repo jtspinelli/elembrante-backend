ed2858960ebc50655e6c345324c9a785608db0a8
jtspinelli <contato@jonathanspinelli.com>
2023-06-17 22:11:29 -0300

    refactor testes unitários para UsuarioUsecases e LembreteUsecases para arquivos únicos

---
+++ tests/app/features/lembrete/usecases/lembreteUsecases.spec.ts
@@ -0,0 +1,229 @@
+import { LembreteRepository } from '../../../../../src/app/features/lembrete/repository';
+import { Lembrete } from '../../../../../src/app/shared/database/entities/Lembrete';
+import { Usuario } from '../../../../../src/app/shared/database/entities/Usuario';
+import { AddLembreteUsecase } from '../../../../../src/app/features/lembrete/usecases/addLembreteUsecase';
+import { GetLembretesUsecase } from '../../../../../src/app/features/lembrete/usecases/getLembretesUsecase';
+import { ArchiveLembreteUsecase } from '../../../../../src/app/features/lembrete/usecases/archiveLembreteUsecase';
+import { RecoverLembreteUsecase } from '../../../../../src/app/features/lembrete/usecases/recoverLembreteUsecase';
+import { RemoveLembreteUsecase } from '../../../../../src/app/features/lembrete/usecases/removeLembreteUsecase';
+import { UpdateLembreteUsecase } from '../../../../../src/app/features/lembrete/usecases/updateLembreteUsecase';
+import LembreteDto from '../../../../../src/app/features/lembrete/dto/LembreteDto';
+
+describe('[LEMBRETE USECASES]', () => {
+	describe('[Add Lembrete Usecase]', () => {
+		const usuario = new Usuario();
+		usuario.nome = 'Mocked';
+		usuario.id = 1;
+		usuario.username = 'mockeduser';
+		
+		const lembrete = new Lembrete();
+		lembrete.descricao = 'Make all tests pass!';
+		lembrete.titulo = 'Run tests';
+		lembrete.id = 1;
+		lembrete.usuario = usuario;
+		
+		const repository = new LembreteRepository();
+		jest.spyOn(repository, 'create').mockResolvedValue(lembrete);
+		
+		const makeSut = () => new AddLembreteUsecase(repository);
+	
+		test('Salva o novo Lembrete chamando \'create\' no Repositório de Lembrete', async () => {
+			const sut = makeSut();
+			jest.spyOn(sut, 'getDto').mockReturnValue(new LembreteDto());
+			
+			await sut.execute('Run tests', 'Make all tests pass!', usuario);
+			
+			expect(repository.create).toBeCalled();
+		});
+		
+		test('Retorna um LembreteDto com as informações do Lembrete criado', async () => {
+			const dto = new LembreteDto();
+			dto.descricao = 'Make all tests pass!';
+			dto.titulo = 'Run tests';
+			
+			const sut = makeSut();
+			jest.spyOn(sut, 'getDto').mockReturnValue(dto);
+			
+			const result = await sut.execute('Run tests', 'Make all tests pass!', usuario);
+			
+			expect(result).toBeInstanceOf(LembreteDto);
+			expect(result.titulo).toBe('Run tests');
+			expect(result.descricao).toBe('Make all tests pass!');
+		});
+	});
+
+	describe('[Get Lembretes Usecase]', () => {	
+		const lembretes: LembreteDto[] = [
+			{
+				titulo: 'Study Testing',
+				descricao: 'Read Jest Docs online',
+				id: 2,
+				arquivado: false,
+				usuarioId: 1,
+				criadoEm: new Date()
+			},
+			{
+				titulo: 'Go Shopping',
+				descricao: 'Buy new suit at Aduana',
+				id: 1,
+				arquivado: false,
+				usuarioId: 1,
+				criadoEm: new Date()
+			}
+		];
+	
+		const repository = new LembreteRepository();
+		jest.spyOn(repository, 'getAll').mockResolvedValue(lembretes);
+	
+		const makeSut = () => new GetLembretesUsecase(repository);
+	
+		test('Chama \'getAll\' no Repositório de Lembretes para obter lista', async () => {
+			const sut = makeSut();
+			await sut.execute(1);
+	
+			expect(repository.getAll).toBeCalled();
+		});
+	
+		test('Retorna lista de LembreteDto', async () => {
+			const sut = makeSut();
+			const result = await sut.execute(1);
+	
+			expect(result.length).toBe(2);
+			result.forEach((lembrete: any) => {
+				expect(typeof lembrete.id).toBe('number');
+				expect(lembrete.titulo).toBeDefined();
+				expect(lembrete.descricao).toBeDefined();
+				expect(typeof lembrete.usuarioId).toBe('number');
+				expect(lembrete.usuario).toBeUndefined();			
+			});
+		});
+	});
+
+	describe('[Archive Lembrete Usecase]', () => {
+		const lembrete = new Lembrete();
+		lembrete.id = 1;
+		lembrete.titulo = 'Hey, ho';
+		lembrete.descricao = 'Let\'s go!';
+		lembrete.usuario = new Usuario();
+		lembrete.arquivado = false;
+	
+		const lembreteArquivado: Lembrete = {...lembrete};
+		lembreteArquivado.arquivado = true;
+		
+		const repository = new LembreteRepository();
+		jest.spyOn(repository, 'findOneById').mockResolvedValue(lembrete);
+		jest.spyOn(repository, 'save').mockResolvedValue(lembreteArquivado);
+	
+		const makeSut = () => new ArchiveLembreteUsecase(repository);
+	
+		test('Chama \'findOneById\' e \'save\' no Repositório de Lembrete', async () => {
+			const sut = makeSut();
+			await sut.execute(1);
+	
+			expect(repository.findOneById).toBeCalled();
+			expect(repository.save).toBeCalled();
+		});
+	
+		test('Retorna lembrete com propriedade \'arquivado\' true', async () => {
+			const sut = makeSut()
+			const result = await sut.execute(1);
+	
+			expect(result?.arquivado).toBeTruthy();
+		});
+	});
+
+	describe('[Recover Lembrete Usecase]', () => {
+		const lembrete = new Lembrete();
+		lembrete.id = 1;
+		lembrete.titulo = 'Hey, ho';
+		lembrete.descricao = 'Let\'s go!';
+		lembrete.usuario = new Usuario();
+		lembrete.arquivado = true;
+	
+		const lembreteRecuperado: Lembrete = {...lembrete};
+		lembreteRecuperado.arquivado = false;
+		
+		const repository = new LembreteRepository();
+		jest.spyOn(repository, 'findOneById').mockResolvedValue(lembrete);
+		jest.spyOn(repository, 'save').mockResolvedValue(lembreteRecuperado);
+	
+		const makeSut = () => new RecoverLembreteUsecase(repository);
+	
+		test('Chama \'findOneById\' e \'save\' no Repositório de Lembrete', async () => {
+			const sut = makeSut();
+			await sut.execute(1);
+	
+			expect(repository.findOneById).toBeCalled();
+			expect(repository.save).toBeCalled();
+		});
+	
+		test('Retorna lembrete com propriedade \'arquivado\' false', async () => {
+			const sut = makeSut()
+			const result = await sut.execute(1);
+	
+			expect(result?.arquivado).toBeFalsy();
+		});
+	});
+
+	describe('[Remove Lembrete Usecase]', () => {
+		const repository = new LembreteRepository();
+		jest.spyOn(repository, 'findOneById').mockResolvedValue(new Lembrete());
+		jest.spyOn(repository, 'remove').mockImplementation((lembrete: Lembrete) => {return new Promise<void>(res => {res()})});
+	
+		const makeSut = () => new RemoveLembreteUsecase(repository);
+	
+		test('Chama \'findOneById\' e \'remove\' no Repositório de Lembrete', async () => {
+			const sut = makeSut();
+			await sut.execute(1);
+	
+			expect(repository.findOneById).toBeCalled();
+			expect(repository.remove).toBeCalled();
+		});
+	});
+
+	describe('[Update Lembrete Usecase]', () => {
+		const lembrete = new Lembrete();
+		lembrete.id = 1;
+		lembrete.titulo = 'Hey, ho';
+		lembrete.descricao = 'Let\'s go!';
+		lembrete.usuario = new Usuario();
+		lembrete.arquivado = true;
+	
+		const updatedLembrete: Lembrete = {...lembrete};
+		updatedLembrete.titulo = 'Hey!';
+		updatedLembrete.descricao = 'Go!';
+		
+		const repository = new LembreteRepository();
+		jest.spyOn(repository, 'findOneByIdWithRelations').mockResolvedValue(lembrete);
+		jest.spyOn(repository, 'save').mockResolvedValue(updatedLembrete);
+	
+		const makeSut = () => new UpdateLembreteUsecase(repository);
+	
+		test('Chama \'findOneByIdWithRelations\' e \'save\' do Repositório de Lembrete', async () => {
+			const dto = new LembreteDto();
+	
+			const sut = makeSut();
+			jest.spyOn(sut, 'getDto').mockReturnValue(dto);
+	
+			await sut.execute(1, 'Hey!', 'Go!');
+	
+			expect(repository.findOneByIdWithRelations).toBeCalled();
+			expect(repository.save).toBeCalled();
+		});
+	
+		test('Retorna LembreteDto', async() => {
+			const dto = new LembreteDto();
+			dto.titulo = 'Hey!';
+			dto.descricao = 'Go!';
+	
+			const sut = makeSut();
+			jest.spyOn(sut, 'getDto').mockReturnValue(dto);
+	
+			const result = await sut.execute(1, 'Hey!', 'Ho!');
+	
+			expect(result).toBeInstanceOf(LembreteDto);
+			expect(result?.titulo).toBe('Hey!');
+			expect(result?.descricao).toBe('Go!');
+		});
+	});
+});
---
+++ tests/app/features/usuario/usecases/usuarioUsecases.spec.ts
@@ -0,0 +1,101 @@
+import { CreateUsuarioUsecase } from '../../../../../src/app/features/usuario/usecases/createUsuarioUsecase';
+import { UsuarioRepository } from '../../../../../src/app/features/usuario/repository';
+import { Usuario } from "../../../../../src/app/shared/database/entities/Usuario";
+import { UpdateUsuarioUsecase } from '../../../../../src/app/features/usuario/usecases/updateUsuarioUsecase';
+import { RemoveUsuarioUsecase } from '../../../../../src/app/features/usuario/usecases/removeUsuarioUsecase';
+import bcrypt from 'bcryptjs';
+
+describe('[USUARIO USECASES]', () => {
+	describe('[Create Usuário Usecase]', () => {
+		const bcryptHash = jest.fn().mockReturnValue('hashedpassword');
+		(bcrypt.hash as jest.Mock) = bcryptHash;
+		
+		const usuario = new Usuario();
+		usuario.nome = 'Mocked';
+		usuario.id = 1;
+		usuario.username = 'mockeduser';
+
+		const mockedCreateTokenResult = {
+			userData: {nome: usuario.nome, username: usuario.username},
+			sign: 'sign',
+			headerPayload: 'header.payload'
+		};
+		
+		const repository = new UsuarioRepository();
+		jest.spyOn(repository,'save').mockResolvedValue(usuario);
+		
+		const makeSut = () => new CreateUsuarioUsecase(repository);
+		
+		test('Salva novo usuário chamando \'save\' no Repositório de Usuário', async () => {
+			const sut = makeSut();
+			jest.spyOn(sut, 'createToken').mockResolvedValue(mockedCreateTokenResult);
+			await sut.execute('Mocked', 'mockeduser', 'pass123*');
+			
+			expect(repository.save).toBeCalled();
+		})
+		
+		test('Retorna userData com Token', async () => {
+			const sut = makeSut();
+			jest.spyOn(sut, 'createToken').mockResolvedValue(mockedCreateTokenResult);
+			const result = await sut.execute('Mocked', 'mockeduser', 'pass123*');
+			
+			expect(result?.userData.nome).toBe('Mocked');
+			expect(result?.headerPayload).toBeDefined();
+			expect(result?.sign).toBeDefined();
+		});
+	});
+
+	describe('[Remove Usuário Usecase]', () => {
+		const usuario = new Usuario();
+		usuario.nome = 'Mocked';
+		usuario.id = 1;
+		usuario.username = 'mockeduser' + ' [Registro excluído] - ' + Math.floor(new Date().getTime() / 1000);;
+		usuario.excluido = true;
+	
+		const repository = new UsuarioRepository();
+		jest.spyOn(repository,'save').mockResolvedValue(usuario);
+	
+		const makeSut = () => new RemoveUsuarioUsecase(repository);
+		
+		test('Salva usuário chamando \'save\' no Repositório de Usuário (exclusão lógica)', async () => {
+			const sut = makeSut();
+			await sut.execute(new Usuario());
+			expect(repository.save).toBeCalled();
+		});
+	
+		test('Retorna o Usuário com \'excluido\' true', async () => {
+			const sut = makeSut();
+			const result = await sut.execute(new Usuario());
+	
+			expect(result).toBeInstanceOf(Usuario);
+			expect(result.excluido).toBeTruthy();
+		});
+	});
+
+	describe('[Update Usuario Usecase]', () => {
+		const usuario = new Usuario();
+		usuario.nome = 'Mocked';
+		usuario.id = 1;
+		usuario.username = 'mockeduser';
+	
+		const repository = new UsuarioRepository();
+		jest.spyOn(repository,'save').mockResolvedValue(usuario);
+	
+		const makeSut = () => new UpdateUsuarioUsecase(repository);
+	
+		test('Salva o usuário chamando \'save\' no Repositório de Usuário', async () => {
+			const sut = makeSut();
+			await sut.execute(usuario, 'Updated Mocked', 'Updated mockeduser');
+	
+			expect(repository.save).toBeCalled();
+		});
+	
+		test('Retorna o usuário com valores atualizados', async () => {
+			const sut = makeSut();
+			const result = await sut.execute(usuario, 'Updated Mocked', 'Updated mockeduser');
+	
+			expect(result.nome).toBe('Updated Mocked');
+			expect(result.username).toBe('Updated mockeduser');
+		});
+	});
+});
