68e879075a4af15146f1052ca49926b1d782c320
jtspinelli <contato@jonathanspinelli.com>
2023-06-24 13:15:29 -0300

    refactorings

--- out/main/config/typeorm.config.js
+++ out/main/config/typeorm.config.js
@@ -4,10 +4,10 @@ const Lembrete_1 = require("../../app/shared/database/entities/Lembrete");
 const Usuario_1 = require("../../app/shared/database/entities/Usuario");
 const _1684598200838_GenerateDb_1 = require("../../app/shared/database/migrations/1684598200838-GenerateDb");
 const _1687053124264_GenerateDb_1 = require("../../app/shared/database/migrations/1687053124264-GenerateDb");
-//dotenv.config();
+const appEnv_1 = require("../../app/env/appEnv");
 let typeormconfig = {
     type: 'postgres',
-    url: process.env.DB_URL,
+    url: appEnv_1.appEnv.dbUrl,
     ssl: {
         rejectUnauthorized: false
     },
